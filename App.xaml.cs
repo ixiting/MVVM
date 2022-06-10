a054f57a4e0bbe3285739a2020c074e8eb45b466
klshv <elizabeth.kuleshova@gmail.com>
2022-06-10 23:41:20 +0700

    Add

---
+++ App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using MVVM_OpenNewWindowMinimalExample.Views;
+using MVVM_OpenNewWindowMinimalExample.ViewModels;
+using System.Threading.Tasks;
+
+namespace MVVM_OpenNewWindowMinimalExample {
+    public partial class App : Application {
+
+        public DisplayRootRegistry displayRootRegistry = new DisplayRootRegistry();
+        MainWindowViewModel mainWindowViewModel;
+
+        public App()
+        {
+            displayRootRegistry.RegisterWindowType<MainWindowViewModel, MainWindowView>();
+            displayRootRegistry.RegisterWindowType<OtherWindowViewModel, ChildWindow>();
+            displayRootRegistry.RegisterWindowType<DialogWindowViewModel, DialogWindow>();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            mainWindowViewModel = new MainWindowViewModel();
+
+            await displayRootRegistry.ShowModalPresentation(mainWindowViewModel);
+
+            Shutdown();
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MVVM_OpenNewWindowMinimalExample.ViewModels
+{
+    class MainWindowViewModel : BasicViewModel
+    {
+        // Закрытые поля команд
+        private ICommand _openChildWindow;
+
+        private ICommand _openDialogWindow;
+
+        // Свойства доступные только для чтения для обращения к командам и их инициализации
+        public ICommand OpenChildWindow
+        {
+            get
+            {
+                if (_openChildWindow == null)
+                {
+                    _openChildWindow = new OpenChildWindowCommand(this);
+                }
+                return _openChildWindow;
+            }
+        }
+        public ICommand OpenDialogWindow
+        {
+            get
+            {
+                if (_openDialogWindow == null)
+                {
+                    _openDialogWindow = new OpenDialogWindowCommand(this);
+                }
+                return _openDialogWindow;
+            }
+        }
+    }
+
+    abstract class MyCommand : ICommand
+    {
+        protected MainWindowViewModel _mainWindowVeiwModel;
+
+        public MyCommand(MainWindowViewModel mainWindowVeiwModel)
+        {
+            _mainWindowVeiwModel = mainWindowVeiwModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public abstract bool CanExecute(object parameter);
+
+        public abstract void Execute(object parameter);
+    }
+
+    class OpenChildWindowCommand : MyCommand
+    {
+        public OpenChildWindowCommand(MainWindowViewModel mainWindowVeiwModel) : base(mainWindowVeiwModel)
+        {
+        }
+        public override bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public override void Execute(object parameter)
+        {
+            var displayRootRegistry = (Application.Current as App).displayRootRegistry;
+
+            var otherWindowViewModel = new OtherWindowViewModel();
+            displayRootRegistry.ShowPresentation(otherWindowViewModel);
+        }
+    }
+
+    class OpenDialogWindowCommand : MyCommand
+    {
+        public OpenDialogWindowCommand(MainWindowViewModel mainWindowVeiwModel) : base(mainWindowVeiwModel)
+        {
+        }
+        public override bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public override async void Execute(object parameter)
+        {
+            var displayRootRegistry = (Application.Current as App).displayRootRegistry;
+
+            var dialogWindowViewModel = new DialogWindowViewModel();
+            await displayRootRegistry.ShowModalPresentation(dialogWindowViewModel);
+
+        }
+    }
+}
